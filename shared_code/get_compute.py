f8b4117360961bccb2dd26575b06feebce03334c
manukanwarpal <36815806+manukanwarpal@users.noreply.github.com>
2021-08-02 17:16:46 +0100

    parameterised get_compute

--- shared_code/get_compute.py
+++ shared_code/get_compute.py
@@ -7,21 +7,30 @@ import shlex
 from azureml.core import Workspace
 from azureml.core.compute import AmlCompute, ComputeTarget
 
-# parser = argparse.ArgumentParser("Get compute")
-# parser.add_argument("-f", type=str, help="Controller Config YAML file")
-# args = parser.parse_args()
+parser = argparse.ArgumentParser("Get compute")
+parser.add_argument("-f", type=str, help="Controller Config YAML file")
+parser.add_argument('--compute_type', type=str, help='Type of compute i.e. training / batch etc.')
+args, _ = parser.parse_known_args()
 
-# with open(args.f, "r") as f:
-#     config = yaml.load(f, Loader=yaml.FullLoader)
-#     config = config['variables']
-# print(config)
+with open(args.f, "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+    config = config['variables']
+print(config)
 
 # choose a name for your cluster
 #config['training_pipeline_target']
-compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME", "cpu-cluster")
-compute_min_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MIN_NODES", 0)
-compute_max_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MAX_NODES", 4)
-
+if args.compute_type == 'training':
+    compute_name = config['training_pipeline_target']
+    compute_min_nodes = config['training_pipeline_target_min_nodes']
+    compute_max_nodes = config['training_pipeline_target_max_nodes']
+elif args.compute_type == 'batch':
+    compute_name = config['batch_pipeline_target']
+    compute_min_nodes = config['batch_pipeline_target_min_nodes']
+    compute_max_nodes = config['batch_pipeline_target_max_nodes']
+else:
+    compute_name = os.environ.get("AML_COMPUTE_CLUSTER_NAME", "cpu-cluster")
+    compute_min_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MIN_NODES", 0)
+    compute_max_nodes = os.environ.get("AML_COMPUTE_CLUSTER_MAX_NODES", 4)
 # This example uses CPU VM. For using GPU VM, set SKU to STANDARD_NC6
 vm_size = os.environ.get("AML_COMPUTE_CLUSTER_SKU", "STANDARD_D2_V2")
 
