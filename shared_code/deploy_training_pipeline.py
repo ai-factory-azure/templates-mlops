9c56ec52c55c172070e3f7af5dc0384b5a1f394b
manukanwarpal <36815806+manukanwarpal@users.noreply.github.com>
2021-08-05 11:53:58 +0100

    dataset

--- shared_code/deploy_training_pipeline.py
+++ shared_code/deploy_training_pipeline.py
@@ -77,9 +77,10 @@ train_step = PythonScriptStep(name="train-step",
                         runconfig=runconfig,
                         compute_target=config['training_pipeline_target'],
                         source_directory="code/src/",
-                        script_name=arguments[0],
+                        #script_name=arguments[0],
                         # arguments=arguments[1:],
                         # inputs=inputs,
+                        script_name= "train.py"
                         arguments=["--data_path", training_dataset_consumption],
                         inputs=training_dataset_consumption,
                         allow_reuse=False)
