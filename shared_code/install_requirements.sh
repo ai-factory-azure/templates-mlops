a0b4f036ecde4971c131f45a7cb1f494622b4876
Manu Kanwarpal <makanw@microsoft.com>
2021-11-13 00:16:01 +0000

    Updated install_requirements.sh

--- shared_code/install_requirements.sh
+++ shared_code/install_requirements.sh
@@ -3,7 +3,9 @@ pip install --upgrade azureml-sdk
 pip install pytest
 pip install pyyaml
 
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y aspnetcore-runtime-6.0
+# sudo apt-get update; \
+#   sudo apt-get install -y apt-transport-https && \
+#   sudo apt-get update && \
+#   sudo apt-get install -y aspnetcore-runtime-6.0
+
+./dotnet-install.sh -c 2.1
