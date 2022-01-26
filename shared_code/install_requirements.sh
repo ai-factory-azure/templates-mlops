e090e4da5286aef10a6903fb67bbd8f04d79dc21
Cindy Weng <8880364+cindyweng@users.noreply.github.com>
2022-01-26 10:58:21 +0000

    Update install_requirements.sh

--- shared_code/install_requirements.sh
+++ shared_code/install_requirements.sh
@@ -3,5 +3,5 @@ pip install azureml-sdk==1.36.0
 pip install pytest
 pip install pyyaml
 pip install azureml-dataset-runtime
-sudo apt install dotnet-runtime-deps-2.1.30
+sudo apt install dotnet-runtime-deps-2.1
 sudo apt install dotnet-runtime-2.1
